64d94058dcaf6fadb36152a055c69444493f7fc0
Nikita <Nik-Mir-Vas@yandex.ru>
2025-01-28 20:55:18 +0300

    Fixed bugs

--- goroutines.go
+++ goroutines.go
@@ -12,8 +12,11 @@ func CyclicReading(
 	out chan<- *string,
 	warn func(string),
 	log func(string),
+	finishCallback func(),
 	fileNames ...string,
 ) {
+	defer finishCallback()
+	defer close(out)
 	files := make([]*os.File, len(fileNames))
 	for i, name := range fileNames {
 		file, err := os.Open(name)
@@ -27,15 +30,17 @@ func CyclicReading(
 	}
 	for _, file := range files {
 		reader := bufio.NewReader(file)
-		line, err := reader.ReadString('\n')
-		if err != nil {
-			if err == io.EOF {
-				break
+		for {
+			line, err := reader.ReadString('\n')
+			if err != nil {
+				if err == io.EOF {
+					break
+				}
+				warn(err.Error())
+				continue
 			}
-			warn(err.Error())
-			continue
+			out <- &line
 		}
-		out <- &line
 	}
 	if !readFromStdIn {
 		log("Finished without STDIN")
@@ -59,9 +64,10 @@ func CyclicReading(
 
 func CyclicWriting(
 	writeToPipeline bool,
-	out <-chan *string,
+	in <-chan *[]byte,
 	warn func(s string),
 	log func(s string),
+	processingStoped func() bool,
 	fileNames ...string,
 ) {
 	writers := make([]*bufio.Writer, len(fileNames))
@@ -76,12 +82,24 @@ func CyclicWriting(
 		defer file.Close()
 	}
 	if writeToPipeline {
-		writers = append(writers, bufio.NewWriter(os.Stdin))
+		writers = append(writers, bufio.NewWriter(os.Stdout))
 	}
-	for {
-		line := <-out
+	defer func() {
 		for _, writer := range writers {
-			nn, err := writer.WriteString(*line)
+			err := writer.Flush()
+			if err != nil {
+				warn(fmt.Sprintf("Writer wasn't flushed: %s", err.Error()))
+			}
+		}
+	}()
+	for i := 0; ; i++ {
+		line, opened := <-in
+		if !opened {
+			log("Channel closed: writing stopped")
+			return
+		}
+		for _, writer := range writers {
+			nn, err := writer.Write(*line)
 			if err != nil {
 				warn(err.Error())
 				continue
@@ -91,19 +109,46 @@ func CyclicWriting(
 				continue
 			}
 			writer.WriteByte('\n')
+			if i >= 100 && i%100 == 0 {
+				err := writer.Flush()
+				if err != nil {
+					warn(fmt.Sprintf("Writer wasn't flushed: %s", err.Error()))
+				}
+			}
+		}
+		if processingStoped() && len(in) == 0 {
+			log("Writing stopped")
+			return
 		}
 	}
 }
 
-func loopRoutine(out chan<- *string, in <-chan *string, warn func(string), function func(*string) (*[]byte, bool)) {
+func loopRoutine(
+	out chan<- *[]byte,
+	in <-chan *string,
+	warn func(string),
+	log func(string),
+	shouldStop func() bool,
+	finishCallback func(),
+	analyzeFunction func(*string) (*[]byte, bool),
+) {
+	defer finishCallback()
 	for {
-		str := <-in
-		st, check := function(str)
+		str, opened := <-in
+		if !opened {
+			log("Channel closed: processing stopped")
+			return
+		}
+		ret, check := analyzeFunction(str)
 		if !check {
 			warn("String wasn't processed")
 			continue
 		}
-		ret := string(*st)
-		out <- &ret
+		out <- ret
+		if shouldStop() {
+			log("Processing stopped")
+			close(out)
+			return
+		}
 	}
 }
